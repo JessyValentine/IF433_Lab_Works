540267cdd83b5693d1c19ed37bd7b991c459ab16
unknown <jessy.valentine.college@gmail.com>
2026-02-06 11:01:21 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000108930_JessyValentine/week01/GameStore.kt
+++ src/oop_00000108930_JessyValentine/week01/GameStore.kt
@@ -5,6 +5,12 @@ fun main(){
     val price: Int = 214999
 
     val finalPrice: Int = calculateDiscount(price)
+
+    printReceipt(title = gameTitle, price = price, finalPrice = finalPrice)
 }
 
-fun calculateDiscount(price: Int): Int = if (price > 500000) price * 20 / 100 else price * 10 / 100
+fun calculateDiscount(price: Int): Int = if (price > 500000) price - (price * 20 / 100) else price - (price * 10 / 100)
+
+fun printReceipt(title: String, price: Int, finalPrice: Int) {
+    println("Judul: $title, Harga Asli: $price, Harga Akhir: $finalPrice")
+}
