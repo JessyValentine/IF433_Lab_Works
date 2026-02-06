8ab0039357bfa6a057487b86416213cfe152f988
unknown <jessy.valentine.college@gmail.com>
2026-02-06 10:50:17 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000108930_JessyValentine/week01/GameStore.kt
+++ src/oop_00000108930_JessyValentine/week01/GameStore.kt
@@ -4,5 +4,7 @@ fun main(){
     val gameTitle: String = "Valheim"
     val price: Int = 214999
 
-
+    val finalPrice: Int = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 500000) price * 20 / 100 else price * 10 / 100
