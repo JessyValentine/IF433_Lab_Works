2215619e144dcfc52801e5abe73c35ba2fab3d01
unknown <jessy.valentine.college@gmail.com>
2026-02-13 11:56:23 +0700

    week02: (task 2) create Hero class

--- src/oop_00000108930_JessyValentine/week02/Hero.kt
+++ src/oop_00000108930_JessyValentine/week02/Hero.kt
@@ -1,7 +1,27 @@
 package oop_00000108930_JessyValentine.week02
 
-class Hero (val name: String, var hp: Int = 100, var baseDamage: Int){
-    fun attack(targetName: String) {
+import oop_00000108930_JessyValentine.week02.Student
+import java.util.Scanner
 
+class Hero (val name: String, var hp: Int, val baseDamage: Int){
+    fun attack(targetName: String){
+        println("$name menebas $targetName")
     }
+
+    fun takeDamage(damage: Int){
+        hp -= damage
+        if (hp < 0){
+            hp = 0
+        }
+    }
+
+    fun isAlive() : Boolean{
+        if (hp > 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    constructor(name: String, baseDamage: Int): this(name, 100, baseDamage)
 }
