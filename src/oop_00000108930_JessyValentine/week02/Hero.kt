dc7efbbd86221faa6f46fe8b601354ff4a719787
unknown <jessy.valentine.college@gmail.com>
2026-02-13 11:59:39 +0700

    week02: (task 2) implement the game

--- src/oop_00000108930_JessyValentine/week02/Hero.kt
+++ src/oop_00000108930_JessyValentine/week02/Hero.kt
@@ -25,3 +25,44 @@ class Hero (val name: String, var hp: Int, val baseDamage: Int){
 
     constructor(name: String, baseDamage: Int): this(name, 100, baseDamage)
 }
+
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    print("Hero Name: ")
+    val name = scanner.nextLine()
+
+    print("Stat Damage: ")
+    val stDamage = scanner.nextInt()
+
+    val hero = Hero(name = name, baseDamage = stDamage)
+
+    var enemyHp = 100
+
+    while(hero.isAlive() && enemyHp > 0){
+        println("Menu: 1. Serang, 2. Kabur")
+        print("Choose: ")
+        val choice = scanner.nextInt()
+        if (choice == 1) {
+            hero.attack("Enemy")
+            enemyHp -= stDamage
+            println("Enemy HP: $enemyHp")
+            if (enemyHp > 0){
+                hero.takeDamage((10..20).random())
+            }
+            println("Hero HP: ${hero.hp}")
+        } else if (choice == 2) {
+            println("Kaburrrrr!!!")
+            break
+        }
+    }
+
+    if(hero.hp > 0 && enemyHp <= 0){
+        println("ANNOUNCEMENT: Hero Menang!")
+    } else if (hero.hp <= 0 && enemyHp > 0){
+        println("ANNOUNCEMENT: Hero Kalah!")
+    } else {
+        println("\nANNOUNCEMENT: Hero Melakukan Tactical Retreat (Bukan pengecut)")
+    }
+}
