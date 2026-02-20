7910760f5318fd1a41669da863f6ab4500eb0ae1
unknown <jessy.valentine.college@gmail.com>
2026-02-20 10:47:18 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000108930_JessyValentine/week03/Main.kt
+++ src/oop_00000108930_JessyValentine/week03/Main.kt
@@ -2,6 +2,16 @@ package oop_00000108930_JessyValentine.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("--- Test Error ---")
-    e.salary = 5000
+
+    // 1. Test Validasi Salary
+    e.salary = -1000
+    e.salary = 500000
+    println("Gaji: ${e.salary}")
+
+    // 2. Test Encapsulation
+    e.increasePerformance()
+    e.performanceRating = 5
+
+    // 3. Test Computed Property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
