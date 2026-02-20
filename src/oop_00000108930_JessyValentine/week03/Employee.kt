16c4b671dec06c830729634e38e28088a3a58cc7
unknown <jessy.valentine.college@gmail.com>
2026-02-20 10:37:38 +0700

    week03: simulate stack overflow error in setter

--- src/oop_00000108930_JessyValentine/week03/Employee.kt
+++ src/oop_00000108930_JessyValentine/week03/Employee.kt
@@ -2,4 +2,9 @@ package oop_00000108930_JessyValentine.week03
 
 class Employee (val name: String) {
     var salary: Int = 0
+        set(value) {
+            println("Mencoba set gaji ke: $value")
+            //Next line ini salah ya. Oke, baik
+            this.salary = value
+        }
 }
---
+++ src/oop_00000108930_JessyValentine/week03/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000108930_JessyValentine.week03
+
+fun main(){
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
