267129f4ee522425b0a1458f1074ed6da88a12ee
unknown <jessy.valentine.college@gmail.com>
2026-02-20 11:19:13 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000108930_JessyValentine/week03/MainTugas.kt
@@ -0,0 +1,8 @@
+package oop_00000108930_JessyValentine.week03
+
+fun main(){
+    val wep = Weapon("Frostner")
+    wep.damage = -50
+    wep.damage = 9999
+    println("Weapon tier: ${wep.tier}")
+}
