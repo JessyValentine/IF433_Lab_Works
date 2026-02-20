7fe2bc4add3c373f90b09aad4ccd172c6d15c621
unknown <jessy.valentine.college@gmail.com>
2026-02-20 10:48:16 +0700

    week03: cleanup code

--- src/oop_00000108930_JessyValentine/week03/Main.kt
+++ src/oop_00000108930_JessyValentine/week03/Main.kt
@@ -3,15 +3,10 @@ package oop_00000108930_JessyValentine.week03
 fun main(){
     val e = Employee("Budi")
 
-    // 1. Test Validasi Salary
-    e.salary = -1000
     e.salary = 500000
     println("Gaji: ${e.salary}")
 
-    // 2. Test Encapsulation
     e.increasePerformance()
-    e.performanceRating = 5
 
-    // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
