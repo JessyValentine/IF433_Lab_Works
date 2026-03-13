ea57f58cae0ee1223db7cbc8e0e30583b4531d0a
unknown <jessy.valentine.college@gmail.com>
2026-03-13 11:05:23 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000108930_JessyValentine/week06/MainSmartHome.kt
@@ -0,0 +1,7 @@
+package oop_00000108930_JessyValentine.week06
+
+fun main(){
+    val lampu = SmartLamp("001", "Ruang Tamu")
+    val speaker = SmartSpeaker("002", "Google Nest Dapur")
+    val watcher = SmartCCTV("003", "Ezviz Garasi")
+}
