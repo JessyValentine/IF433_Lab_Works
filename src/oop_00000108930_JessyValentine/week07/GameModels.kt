d47145eeb42ed03429b08e3b7522956b377a24a0
unknown <jessy.valentine.college@gmail.com>
2026-03-27 11:09:32 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000108930_JessyValentine/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000108930_JessyValentine.week07
+
+enum class ItemRarity (val dropChance: Int){
+    COMMON (70),
+    UNCOMMON (60),
+    RARE (30),
+    EPIC (10),
+    LEGENDARY (5)
+}
