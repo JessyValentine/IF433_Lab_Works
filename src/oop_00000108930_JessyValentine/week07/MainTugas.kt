86d5fc9bbc51a07448e7e7ee444d396f788a49d8
unknown <jessy.valentine.college@gmail.com>
2026-03-27 11:26:49 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000108930_JessyValentine/week07/MainTugas.kt
+++ src/oop_00000108930_JessyValentine/week07/MainTugas.kt
@@ -3,4 +3,13 @@ package oop_00000108930_JessyValentine.week07
 fun main(){
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\nLegendary Item Drop Chance: ${ItemRarity.LEGENDARY.dropChance}")
+    val wep = Weapon.forgeStarterSword()
+
+    println("\n=== NEW STARTER WEAPON ===")
+    println("Weapon name: ${wep.item.name}")
+    println("Damage: ${wep.item.damage}")
+    println("Rarity: ${wep.item.rarity}")
+    println("Durability: ${wep.durability}")
 }
