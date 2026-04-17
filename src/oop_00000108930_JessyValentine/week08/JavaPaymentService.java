89691ec4e1167d3e52f4d7737640a3183c2fe433
unknown <jessy.valentine.college@gmail.com>
2026-04-17 13:08:58 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000108930_JessyValentine/week08/JavaPaymentService.java
@@ -0,0 +1,5 @@
+package oop_00000108930_JessyValentine.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){return "TRX-" + productId + "-SUCCESS";}
+}
