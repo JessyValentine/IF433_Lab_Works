42af15be1daca607de71ec8d0cc08f8adf399d7e
unknown <jessy.valentine.college@gmail.com>
2026-04-17 13:08:31 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_00000108930_JessyValentine/week08/ApiParser.kt
@@ -0,0 +1,20 @@
+package oop_00000108930_JessyValentine.week08
+
+class ApiParser {
+    fun parseProduct (rawJson: Map<String, Any?>): Product?{
+        val id = requireNotNull(rawJson["id"]){
+            "API Invalid: Missing ID"
+        }
+        val name = requireNotNull(rawJson["name"]){
+            "API Invalid: Missing Name"
+        }
+        val type = rawJson["type"].toString()
+        if(type == "ELECTRONIC"){
+            val wrnty = rawJson["warranty"] as? Int ?: 12
+            return Product.Electronic(id.toString(), name.toString(), wrnty)
+        }else if(type == "CLOTHING"){
+            val size = rawJson["size"] as? String ?: "All Size"
+            return Product.Clothing(id.toString(), name.toString(), size)
+        }else return null
+    }
+}
