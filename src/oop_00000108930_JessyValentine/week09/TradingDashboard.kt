062b17446adfafc8f26a75cc8920111285c9ada9
unknown <jessy.valentine.college@gmail.com>
2026-04-24 10:31:52 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000108930_JessyValentine/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000108930_JessyValentine.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTC/USDT", "LONG", 20, 5.0, "OPEN"),
+        TradeLog("ETH/USDT", "SHORT", 7, 10.0, "CLOSED"),
+        TradeLog("BTC/USDT", "LONG", 80, -4.0, "CLOSED"),
+        TradeLog("ETH/USDT", "LONG", 10, 5.0, "OPEN"),
+        TradeLog("SOL/USDT", "SHORT", 8, -7.0, "CLOSED"),
+        TradeLog("XRP/USDT", "SHORT", 6, -9.0, "OPEN")
+    )
+}
