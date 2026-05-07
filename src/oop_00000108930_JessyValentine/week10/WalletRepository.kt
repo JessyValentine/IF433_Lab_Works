9cf99cf6c58a5575d70d17947ad066b8e8f3ab0f
unknown <jessy.valentine.college@gmail.com>
2026-05-07 12:37:35 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000108930_JessyValentine/week10/WalletRepository.kt
+++ src/oop_00000108930_JessyValentine/week10/WalletRepository.kt
@@ -9,4 +9,9 @@ class WalletRepository<T>{
     fun getAll():List<T>{
         return items
     }
+
+    fun <T: Any> search(nama: T){
+        val result = items.filter{it == nama}
+        println("Hasil pencarian: $result")
+    }
 }
