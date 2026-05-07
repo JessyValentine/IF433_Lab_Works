d42a30e596c821673445f22e23339846e562c489
unknown <jessy.valentine.college@gmail.com>
2026-05-07 12:38:29 +0700

    week10: (task) populate coin repository

--- src/oop_00000108930_JessyValentine/week10/CryptoDashboard.kt
+++ src/oop_00000108930_JessyValentine/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000108930_JessyValentine.week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("ETH", 3.14))
+    coinRepo.add(Coin("BTC", 1.2))
+    coinRepo.add(Coin("USDT", 2.1))
 }
