75814e5e417f412d6bbe68a43ab7718237f90c93
unknown <jessy.valentine.college@gmail.com>
2026-05-07 13:10:10 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000108930_JessyValentine/week10/CryptoDashboard.kt
+++ src/oop_00000108930_JessyValentine/week10/CryptoDashboard.kt
@@ -9,5 +9,10 @@ fun main(){
     val response = ApiResponse("200 OK", coinRepo.getAll())
     println(response.status)
 
-    response.data.forEach { println("Name: ${it.name} Balance: ${it.balance}") }
+    response.data.forEach { println("Coin Name: ${it.name} Balance: ${it.balance}") }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("1", 1.4))
+    txRepo.add(Transaction("2", 1.1))
+    txRepo.add(Transaction("3", 2.2))
 }
