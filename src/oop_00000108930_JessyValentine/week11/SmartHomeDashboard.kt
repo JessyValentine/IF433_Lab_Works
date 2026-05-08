5352dc3c7cafde648a2f57ca6df1c1e657e0dd3c
unknown <jessy.valentine.college@gmail.com>
2026-05-08 10:35:01 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000108930_JessyValentine/week11/SmartHomeDashboard.kt
+++ src/oop_00000108930_JessyValentine/week11/SmartHomeDashboard.kt
@@ -1,11 +1,19 @@
 package oop_00000108930_JessyValentine.week11
 
-fun main(){
+fun main() {
     val homeDevices = mutableListOf<SmartDevice>()
-    val lampuPintar = SmartDevice("Philips WiZ Living Room", "Lighting").apply{
+    val lampuPintar = SmartDevice("Philips WiZ Living Room", "Lighting").apply {
         isOnline = true
         powerLoad = 12
-    }.also{
+    }.also {
+        homeDevices.add(it)
+    }
+
+    val perangkat = SmartDevice("Ezviz Outdoor", "Camera").apply {
+        isOnline = true
+        powerLoad = 5
+    }.also {
+        println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
 }
